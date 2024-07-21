710103e6803b325ca4185f8565abc62211fe3f10
unknown <chanon.utupon@gmail.com>
2024-07-21 15:17:53 +0700

    update_submit_file

--- scripts/tokenizer/huggingface/submit_train.sh
+++ scripts/tokenizer/huggingface/submit_train.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH -p memory                          # Specify partition [Compute/Memory/GPU]
-#SBATCH -N 1 -c 128                     # Specify number of nodes and processors per task
-#SBATCH --ntasks-per-node=1		# Specify number of tasks per node
-#SBATCH --gpus-per-node=0		        # Specify total number of GPUs
-#SBATCH -t 120:00:00                     # Specify maximum time limit (hour: minute: second)
-#SBATCH -A <project_name>                     # Specify project name
-#SBATCH -J sentencepiece_training                          # Specify job name
+#SBATCH -p memory                               # Specify partition [Compute/Memory/GPU]
+#SBATCH -N 1 -c 128                             # Specify number of nodes and processors per task
+#SBATCH --ntasks-per-node=1		                # Specify number of tasks per node
+#SBATCH --gpus-per-node=0		                # Specify total number of GPUs
+#SBATCH -t 120:00:00                            # Specify maximum time limit (hour: minute: second)
+#SBATCH -A <project_name>                       # Specify project name
+#SBATCH -J huggingface_tokenizer_training       # Specify job name
 
 ml restore
 ml Mamba
