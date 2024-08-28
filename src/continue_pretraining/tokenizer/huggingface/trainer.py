2e1eb8eb79f3223442fd83767c28cda94eb6f87c
unknown <chanon.utupon@gmail.com>
2024-08-28 14:49:39 +0700

    undo_hf_trainer

--- src/continue_pretraining/tokenizer/huggingface/trainer.py
+++ src/continue_pretraining/tokenizer/huggingface/trainer.py
@@ -1,83 +1,98 @@
-import os
+from tokenizers import ByteLevelBPETokenizer
+from datasets import load_from_disk, load_dataset, Dataset
 from typing import Optional
-import sentencepiece as spm
-from transformers import LlamaTokenizer
-from datasets import Dataset, load_dataset, load_from_disk
-
-from continue_pretraining.tokenizer.sentencepiece.constants import (
-    TEXT_COLUMN,
-    EOS_TOKEN,
-    BOS_TOKEN,
-    UNK_TOKEN,
-    UNIGRAM_MODE,
-    WORD_MODE,
-    CHAR_MODE,
-    BPE_MODE,
-    TRAIN_SPLIT,
-)
 from tqdm import tqdm
+import os
+
+from continue_pretraining.tokenizer.huggingface.constants import (
+    TRAIN_SPLIT,
+    TEXT_COLUMN,
+    BOS_TOKEN,
+    EOS_TOKEN,
+    UNK_TOKEN,
+)
 
 
-def dataset_iterator(dataset: Dataset, text_column: str):
+def dataset_batch_iterator(
+    dataset: Dataset,
+    text_column: str,
+    batch_size: int = 1000,
+):
     """
-    A generator function that iterates over a dataset and yields the text data from the specified column.
-
-    ฟังก์ชัน generator สำหรับวนซ้ำ dataset และคืนค่าข้อมูลข้อความจากคอลัมน์ที่กำหนด
+    A generator function that iterates over a dataset in batches and yields lists of text data from the specified column.
 
     Args:
-        dataset (Dataset): dataset ที่จะวนซ้ำ
-        text_column (str): ชื่อของคอลัมน์ที่มีข้อมูลข้อความ
+        dataset (Dataset): The dataset to iterate over.
+        text_column (str): The name of the column containing the text data.
+        batch_size (int, optional): The number of rows to include in each batch. Defaults to 1000.
 
     Yields:
-        str: ข้อมูลข้อความจากคอลัมน์ที่กำหนดสำหรับแต่ละแถวใน dataset
-    """  # noqa: E501
-    # Iterate over the dataset using tqdm to show a progress bar
-    # วนซ้ำ dataset โดยใช้ tqdm เพื่อแสดง progress bar
-    for i in tqdm(range(len(dataset))):
-        # Yield the text data from the specified column
-        # คืนค่าข้อมูลข้อความจากคอลัมน์ที่กำหนด
-        yield dataset[i][text_column]
+        List[str]: A list of text data from the specified column for each batch in the dataset.
 
+    ---
 
-def train_tokenizer(
-    output_path: str,
-    vocab_size: int,
-    num_threads: Optional[int] = os.cpu_count(),
-    load_dataset_path: str = "oscar",
-    load_dataset_name: Optional[str] = None,
-    is_local: bool = False,
-    large_corpus: bool = False,
-    mode: str = BPE_MODE,
-) -> None:
-    """
-    Train a SentencePiece tokenizer on a large text dataset.
-
-    ฝึก SentencePiece tokenizer บน dataset ขนาดใหญ่
+    ฟังก์ชัน generator ที่วนลูปผ่าน dataset เป็น batch และคืนค่ารายการของข้อมูล text จากคอลัมน์ที่ระบุ
 
     Args:
-        output_path (str): เส้นทางและคำนำหน้าที่ใช้ในการบันทึก tokenizer ที่ฝึกแล้ว
-        vocab_size (int): ขนาดของ vocabulary ที่จะใช้ในการฝึก tokenizer
-        num_threads (int, optional): จำนวน threads ที่จะใช้ในการฝึก tokenizer ค่าเริ่มต้นคือจำนวน cores CPU ที่ใช้ได้
-        load_dataset_path (str, optional): ชื่อหรือเส้นทางของ Hugging Face dataset ที่จะโหลด ค่าเริ่มต้นคือ "oscar"
-        load_dataset_name (str, optional): ชื่อของ dataset split ที่จะใช้ ค่าเริ่มต้นคือ None
-        is_local (bool): ระบุว่าข้อมูลอยู่ในโฟลเดอร์ local หรือไม่ ค่าเริ่มต้นคือ False
-        large_corpus (bool): ระบุว่ากำลังรันโค้ดบน dataset ขนาดใหญ่หรือไม่ ค่าเริ่มต้นคือ False
-        mode (str): โหมดการฝึก tokenizer ค่าเริ่มต้นคือ 'bpe'
+        dataset (Dataset): dataset ที่จะวนลูป
+        text_column (str): ชื่อของคอลัมน์ที่มีข้อมูล text
+        batch_size (int, optional): จำนวนแถวในแต่ละ batch ขนาดเริ่มต้นเป็น 1000
+
+    Yields:
+        List[str]: รายการของข้อมูล text จากคอลัมน์ที่ระบุสำหรับแต่ละ batch ใน dataset
+    """  # noqa: E501
+    # Iterate over the dataset in steps of batch_size using tqdm to show a progress bar  # noqa: E501
+    # วนลูปผ่าน dataset เป็นช่วงของ batch_size โดยใช้ tqdm แสดงแถบความก้าวหน้า
+    for i in tqdm(range(0, len(dataset), batch_size)):
+        # Yield a list of text data from the specified column for the current batch  # noqa: E501
+        # คืนค่ารายการของข้อมูล text จากคอลัมน์ที่ระบุสำหรับ batch ปัจจุบัน
+        yield [
+            text for text in dataset[i : i + batch_size][text_column]  # noqa: E203 E501
+        ]
+
+
+def train(
+    output_path: str,
+    load_dataset_path: str,
+    load_dataset_name: Optional[str] = None,
+    is_local: bool = False,
+    batch_size: int = 1000,
+    vocab_size: int = 32000,
+) -> None:
+    """
+    Train a ByteLevelBPETokenizer on a large text dataset.
+
+    Args:
+        output_path (str): The path to use when saving the trained tokenizer.
+        load_dataset_path (str): The name or path of the Hugging Face dataset to load.
+        load_dataset_name (Optional[str]): The name of the dataset split to use. Defaults to None.
+        is_local (bool): Whether the dataset is in a local directory. Defaults to False.
+        batch_size (int): The size of the batch to use when training the tokenizer. Defaults to 1000.
+        vocab_size (int): The size of the vocabulary to use when training the tokenizer. Defaults to 32000.
+
+    Returns:
+        None
+
+    ---
+
+    ฝึกฝน ByteLevelBPETokenizer บน dataset ข้อความขนาดใหญ่
+
+    Args:
+        output_path (str): เส้นทางที่ใช้เมื่อบันทึก tokenizer ที่ฝึกฝนแล้ว
+        load_dataset_path (str): ชื่อหรือ path ของ Hugging Face dataset ที่จะโหลด
+        load_dataset_name (Optional[str]): ชื่อของ dataset split ที่จะใช้ ค่าเริ่มต้นเป็น None
+        is_local (bool): ว่า dataset อยู่ใน directory ภายในเครื่องหรือไม่ ค่าเริ่มต้นเป็น False
+        batch_size (int): ขนาดของ batch ที่ใช้ในการฝึกฝน tokenizer ค่าเริ่มต้นเป็น 1000
+        vocab_size (int): ขนาดของ vocabulary ที่ใช้ในการฝึกฝน tokenizer ค่าเริ่มต้นเป็น 32000
 
     Returns:
         None
     """  # noqa: E501
-    # Validate mode argument
-    # ตรวจสอบค่า argument mode
-    if mode not in {UNIGRAM_MODE, BPE_MODE, WORD_MODE, CHAR_MODE}:
-        raise KeyError(
-            f"mode ต้องเป็นหนึ่งใน {UNIGRAM_MODE}, {WORD_MODE}, {CHAR_MODE}, หรือ {BPE_MODE}"  # noqa: E501
-        )
 
-    # Load dataset from Hugging Face if not local
-    # โหลด dataset จาก Hugging Face หากไม่ได้อยู่ใน local
+    # Load the dataset
+    # โหลด dataset
     if not is_local:
-        text_dataset = load_dataset(
+        dataset = load_dataset(
             path=load_dataset_path,
             name=load_dataset_name,
             split=TRAIN_SPLIT,
@@ -86,31 +101,23 @@ def train_tokenizer(
     else:
         # Load dataset from local disk
         # โหลด dataset จาก local disk
-        text_dataset = load_from_disk(load_dataset_path)[TRAIN_SPLIT]
+        dataset = load_from_disk(load_dataset_path)[TRAIN_SPLIT]
 
-    # Ensure the output directory exists
-    # ตรวจสอบว่าโฟลเดอร์สำหรับบันทึกผลลัพธ์มีอยู่หรือไม่ หากไม่มีให้สร้าง
-    os.makedirs(output_path, exist_ok=True)
+    # Instantiate the ByteLevelBPETokenizer
+    # สร้าง ByteLevelBPETokenizer
+    tokenizer = ByteLevelBPETokenizer()
 
-    # Train SentencePiece tokenizer
-    # ฝึก SentencePiece tokenizer
-    spm.SentencePieceTrainer.train(
-        sentence_iterator=iter(dataset_iterator(text_dataset, TEXT_COLUMN)),
-        model_prefix=output_path + "/spm_tokenizer",
+    # Train the tokenizer on the dataset
+    # ฝึกฝน tokenizer บน dataset
+    tokenizer.train_from_iterator(
+        dataset_batch_iterator(dataset, TEXT_COLUMN, batch_size),
         vocab_size=vocab_size,
-        user_defined_symbols=[],
-        num_threads=num_threads,
-        train_extremely_large_corpus=large_corpus,
-        model_type=mode,
+        min_frequency=2,
+        special_tokens=[BOS_TOKEN, EOS_TOKEN, UNK_TOKEN],
     )
 
-    # Load and configure the tokenizer
-    # โหลดและตั้งค่า tokenizer
-    tokenizer = LlamaTokenizer(vocab_file=output_path + "/spm_tokenizer.model")
-    tokenizer.eos_token = EOS_TOKEN
-    tokenizer.bos_token = BOS_TOKEN
-    tokenizer.unk_token = UNK_TOKEN
-
-    # Save the tokenizer
-    # บันทึก tokenizer
-    tokenizer.save_pretrained(output_path)
+    # Save the trained tokenizer to disk
+    # บันทึก tokenizer ที่ฝึกฝนแล้วไปยัง disk
+    os.makedirs(output_path, exist_ok=True)
+    tokenizer.save(output_path + "/tokenizer.json")
+    tokenizer.save_model(output_path)
